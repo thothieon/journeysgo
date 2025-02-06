8d7d6b06f348e57d60f198629426d63e916706e6
thothieon <thothieon@hotmail.com>
2025-02-07 01:59:49 +0800

    增加測試column code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="home">
     <div>
-    <Navbar />
+      <Navbar />
     </div>
     <div>
       <h1>This is an home page</h1>
+      <br />
+      <div class="container px-4">
+        <div class="row gx-5">
+          <div class="col">
+            <div class="p-3 border bg-light">Custom column padding</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Custom column padding</div>
+          </div>
+        </div>
+      </div>
+      <br />
     </div>
     <div>
-    <Footer />
+      <Footer />
     </div>
   </div>
 </template>
