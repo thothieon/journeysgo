9b1511540247d170c2765bc0fb67a8b99a2edf30
thothieon <thothieon@hotmail.com>
2025-02-08 15:45:44 +0800

    Card 測試版本

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,14 +7,33 @@
       <h1>This is an home page</h1>
       <br />
       <div class="container">
-        <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <br />
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col" v-for="(index, item) in productList">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">{{ index }}</h5>
+                <p class="card-text">{{ item }}</p>
+              </div>
+            </div>
           </div>
         </div>
+        <br />
+        <hr>
+        <br />
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col" v-for="(index, item) in productList01">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">index : {{ index }} - key : {{ item.name }}</h5>
+                <p class="card-text">{{item.age}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
       </div>
       <br />
     </div>
@@ -34,6 +53,18 @@ export default {
     Navbar,
     // eslint-disable-next-line vue/no-reserved-component-names
     Footer
+  },
+  data() {
+    return {
+      productList: ["aaa", "bbb", "ccc", "ddd"],
+      productList01:[
+        {name: 'John', age: 24},
+        {name: 'John01', age: 241},
+        {name: 'John02', age: 242},
+        {name: 'John03', age: 243},
+
+      ]
+    }
   }
 }
 </script>
